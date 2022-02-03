e46e8ea8fa37c1ff21321886d8471c1caea5d4f5
Alexander Hunt <alexander.hunt@ed.ac.uk>
2022-02-03 10:06:11 +0000

    update

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-./build_release/darknet detector train cfg/names.data cfg/yolov4.cfg 
+./build_release/darknet detector map train data/train/_darknet.labels cfg/yolov4.cfg weights.file -json_port 8070 -mjpeg_port 8090 
